76d6e8a7f8f0f91ff5669de321f19f76ad8540b2
Carlos Siu <Siumauricio@unitec.edu>
2021-12-04 10:03:02 +0000

    config and changes done

--- Pair-Programming/Calculator.cs
+++ Pair-Programming/Calculator.cs
@@ -7,18 +7,9 @@ using System.Threading.Tasks;
 namespace Pair_Programming {
    public class Calculator {
 
-        public int Sum(string number="0") {
+        public int Sum(string number="25") {
             if (number.Contains(",")) {
                 string[] values = number.Split(",");
-                //string[] newLine = new string[values.Length];
-                //string[] result_values = values.ToList()
-
-                // for (int i = 0; i < values.Length; i++) {
-                //     //if (values[i]) {
-
-                //     //}
-                //     values[i].Split("\n");
-                // }
                 int result = 0;
                 for (int i = 0; i < values.Length; i++) {
                     result += Int32.Parse(values[i]);
