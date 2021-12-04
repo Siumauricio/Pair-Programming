736c92163187068bca50ce3a927c32bb73d7b0dd
Carlos Siu <Siumauricio@unitec.edu>
2021-12-04 09:12:49 +0000

    config

--- Pair-Programming/UnitTest1.cs
+++ Pair-Programming/UnitTest1.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-
 namespace Pair_Programming {
     public class Tests {
         [SetUp]
@@ -7,8 +6,30 @@ namespace Pair_Programming {
         }
 
         [Test]
-        public void Test1() {
-            Assert.Pass();
+        public void User_Input_Is_Empty() {
+            // Arrange
+            var calc = new Calculator();
+            var expect = 0;
+
+            // Act
+            var result = calc.Sum();
+
+            // Assert
+            Assert.AreEqual(expect, result);
+
+        }
+         [Test]
+        public void User_Input_Is_One_Number_Should_Return_Same_Number() {
+            // Arrange
+            var calc = new Calculator();
+            var expect = 3;
+
+            // Act
+            var result = calc.Sum("3");
+
+            // Assert
+            Assert.AreEqual(expect, result);
+
         }
     }
 }
